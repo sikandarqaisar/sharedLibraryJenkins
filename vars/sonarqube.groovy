9221f37c6f8bcc437bbda9b804bd4c74c30db157
sikandarqaisar <50927206+sikandarqaisar@users.noreply.github.com>
2019-10-17 05:33:13 +0500

    Update sonarqube.groovy

--- vars/sonarqube.groovy
+++ vars/sonarqube.groovy
@@ -5,11 +5,8 @@ node {
    def sonarUrl = 'sonar.host.url=http://3.19.245.190:9000'
    def mvn = tool (name: 'maven3', type: 'maven') + '/bin/mvn'
    stage('SCM Checkout'){
-    // Clone repo
 	git branch: gitBranch, 
-    //	credentialsId: 'github', 
-	url: Url
-   
+	url: Url  
    }
    
    stage('Sonar Publish'){
@@ -19,27 +16,7 @@ node {
 	 }
       
    }
-   
-	
-//   stage('Mvn Package'){
-//	   // Build using maven
-	   
-//	   sh "${mvn} clean package deploy"
- //  }
-   
-//   stage('deploy-dev'){
- //      def tomcatDevIp = '172.31.28.172'
-//	   def tomcatHome = '/opt/tomcat8/'
-//	   def webApps = tomcatHome+'webapps/'
-//	   def tomcatStart = "${tomcatHome}bin/startup.sh"
-//	   def tomcatStop = "${tomcatHome}bin/shutdown.sh"
-	   
-//	   sshagent (credentials: ['tomcat-dev']) {
-//	      sh "scp -o StrictHostKeyChecking=no target/myweb*.war ec2-user@${tomcatDevIp}:${webApps}myweb.war"
-  //        sh "ssh ec2-user@${tomcatDevIp} ${tomcatStop}"
-	//	  sh "ssh ec2-user@${tomcatDevIp} ${tomcatStart}"
-  //     }
- //  }
+
    stage('Email Notification'){
 		mail bcc: '', body: """Hi Team, You build successfully deployed
 		                       Job URL : ${env.JOB_URL}
