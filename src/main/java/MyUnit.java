793c66c68773a90910a813019af31290711a8700
sikandarqaisar <50927206+sikandarqaisar@users.noreply.github.com>
2019-10-18 23:52:19 +0500

    Create MyUnit.java

---
+++ src/main/java/MyUnit.java
@@ -0,0 +1,11 @@
+package com.coveros.demo.helloworld;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class MyUnit {
+
+    public String concatenate(String one, String two){
+        return one + two;
+    }
+}
