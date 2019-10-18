eda7ee91b19e5bc254239b015cfa370e12666459
sikandarqaisar <50927206+sikandarqaisar@users.noreply.github.com>
2019-10-18 23:07:47 +0500

    Update MathTest.java

--- src/test/java/MathTest.java
+++ src/test/java/MathTest.java
@@ -2,10 +2,21 @@ import org.junit.Assert;
 import org.junit.Before;
 import static org.junit.Assert.*;
 import org.junit.Test;
+
 public class MathTest {
-    
-    @Test
-    public void passTest() {
-        assertTrue( true );
+    Math math;
+    @Before
+    public void setUp() throws Exception {
+        math = new Math(7, 10);
     }
+    @Test
+    public void testAdd() {
+        Assert.assertEquals(17, math.add());
+    }
+    
+//     @Test
+//     public void passTest() {
+//         assertTrue( true );
+//     }
 }
+
