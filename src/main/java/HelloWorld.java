286bfe65a45c54f798f6523e40fc91bb73b61504
sikandarqaisar <50927206+sikandarqaisar@users.noreply.github.com>
2019-10-18 23:50:31 +0500

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,21 +3,27 @@ package com.coveros.demo.helloworld;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class HelloWorld {
+public class MyUnit {
 
-  public static void main(final String[] args) {
-    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
-    final LocalDateTime now = LocalDateTime.now();
-
-    System.out.println("Hello, World! The current time is " + dtf.format(now));
-  }
-  
-  public int add(int a, int b) {
-      return a + b;
-  }
-  public int multiple(int a, int b) {
-        return a * b;
+    public String concatenate(String one, String two){
+        return one + two;
     }
+}
+// public class HelloWorld {
+
+//   public static void main(final String[] args) {
+//     final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
+//     final LocalDateTime now = LocalDateTime.now();
+
+//     System.out.println("Hello, World! The current time is " + dtf.format(now));
+//   }
+  
+//   public int add(int a, int b) {
+//       return a + b;
+//   }
+//   public int multiple(int a, int b) {
+//         return a * b;
+//     }
   
 
-}
+// }
