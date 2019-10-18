f892aa11f96f67b1d200ca07874154e4bee9cad8
sikandarqaisar <50927206+sikandarqaisar@users.noreply.github.com>
2019-10-19 00:13:55 +0500

    Update MyUnit.java

--- src/main/java/MyUnit.java
+++ src/main/java/MyUnit.java
@@ -1,11 +1,11 @@
-package com.coveros.demo.helloworld;
+// package com.coveros.demo.helloworld;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+// import java.time.LocalDateTime;
+// import java.time.format.DateTimeFormatter;
 
-public class MyUnit {
+// public class MyUnit {
 
-    public String concatenate(String one, String two){
-        return one + two;
-    }
-}
+//     public String concatenate(String one, String two){
+//         return one + two;
+//     }
+// }
