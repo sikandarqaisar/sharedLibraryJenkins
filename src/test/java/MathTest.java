1681251b617d7bccbe1aee58db5c2fc1eb6c6b1c
sikandarqaisar <50927206+sikandarqaisar@users.noreply.github.com>
2019-10-18 23:56:26 +0500

    Update MathTest.java

--- src/test/java/MathTest.java
+++ src/test/java/MathTest.java
@@ -27,10 +27,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MyUnitTest {
-
+    MyUnit myUnit;
     @Test
     public void testConcatenate() {
-        MyUnit myUnit = new MyUnit();
+        myUnit = new MyUnit();
 
         String result = myUnit.concatenate("one", "two");
 
