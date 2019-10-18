7d98436702e4954c5eef888aca9b76af18684818
sikandarqaisar <50927206+sikandarqaisar@users.noreply.github.com>
2019-10-19 00:23:37 +0500

    Update MathTest.java

--- src/test/java/MathTest.java
+++ src/test/java/MathTest.java
@@ -1,25 +1,25 @@
-// package com.coveros.demo.helloworld;
-// // import org.junit.Assert;
-// // import org.junit.Before;
-// // import static org.junit.Assert.*;
-// // import org.junit.Test;
-// // public class MathTest {
-// //     HelloWorld math;
-// //     @Before
-// //     public void setUp() throws Exception {
-// //          math = new HelloWorld();
+package com.coveros.demo.helloworld;
+import org.junit.Assert;
+import org.junit.Before;
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class MathTest {
+    HelloWorld math;
+    @Before
+    public void setUp() throws Exception {
+         math = new HelloWorld();
 
-// //     }
-// //     @Test
-// //     public void testAdd() {
-// //         Assert.assertEquals(17, math.add(7 , 10));
-// //     }
-// //         public void testMulti() {
-// //         Assert.assertEquals(15, math.multiple(5 , 3));
-// //     } 
+    }
+    @Test
+    public void testAdd() {
+        Assert.assertEquals(17, math.add(7 , 10));
+    }
+//         public void testMulti() {
+//         Assert.assertEquals(15, math.multiple(5 , 3));
+//     } 
     
     
-// // }
+}
     
 
 // import org.junit.Test;
@@ -47,20 +47,20 @@
 
     
     
-// //     @Test
-// //     public void passTest() {
-// //         assertTrue( true );
-// //     }
+//     @Test
+//     public void passTest() {
+//         assertTrue( true );
+//     }
 
 
-// // public class MathTest {
-// //     Math math;
-// //     @Before
-// //     public void setUp() throws Exception {
-// //         math = new Math(7, 10);
-// //     }
-// //     @Test
-// //     public void testAdd() {
-// //         Assert.assertEquals(17, math.add());
-// //     }
-// // }
+// public class MathTest {
+//     Math math;
+//     @Before
+//     public void setUp() throws Exception {
+//         math = new Math(7, 10);
+//     }
+//     @Test
+//     public void testAdd() {
+//         Assert.assertEquals(17, math.add());
+//     }
+// }
