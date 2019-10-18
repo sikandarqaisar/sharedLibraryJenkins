a58a708cda9ea5245ff7a8b5d9ce0c29eb2073fe
sikandarqaisar <50927206+sikandarqaisar@users.noreply.github.com>
2019-10-18 23:49:26 +0500

    Update MathTest.java

--- src/test/java/MathTest.java
+++ src/test/java/MathTest.java
@@ -1,34 +1,57 @@
 package com.coveros.demo.helloworld;
-import org.junit.Assert;
-import org.junit.Before;
-import static org.junit.Assert.*;
+// import org.junit.Assert;
+// import org.junit.Before;
+// import static org.junit.Assert.*;
+// import org.junit.Test;
+// public class MathTest {
+//     HelloWorld math;
+//     @Before
+//     public void setUp() throws Exception {
+//          math = new HelloWorld();
+
+//     }
+//     @Test
+//     public void testAdd() {
+//         Assert.assertEquals(17, math.add(7 , 10));
+//     }
+//         public void testMulti() {
+//         Assert.assertEquals(15, math.multiple(5 , 3));
+//     } 
+    
+    
+// }
+    
+
 import org.junit.Test;
-public class MathTest {
-    HelloWorld math;
-    @Before
-    public void setUp() throws Exception {
-         math = new HelloWorld();
+import static org.junit.Assert.*;
+
+public class MyUnitTest {
+
+    @Test
+    public void testConcatenate() {
+        MyUnit myUnit = new MyUnit();
+
+        String result = myUnit.concatenate("one", "two");
+
+        assertEquals("onetwo", result);
 
     }
-    @Test
-    public void testAdd() {
-        Assert.assertEquals(17, math.add(7 , 10));
-    }
-        public void testMulti() {
-        Assert.assertEquals(15, math.multiple(5 , 3));
-    } 
-    
-    
-    
-    
-    
+}
+
+
+
+
+
+
+
+
     
     
 //     @Test
 //     public void passTest() {
 //         assertTrue( true );
 //     }
-}
+
 
 // public class MathTest {
 //     Math math;
