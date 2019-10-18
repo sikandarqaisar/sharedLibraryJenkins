e4fc52b5d7eb48028e875b20a9825f2fd056561b
sikandarqaisar <50927206+sikandarqaisar@users.noreply.github.com>
2019-10-18 22:46:21 +0500

    Update sonarqube.groovy

--- vars/sonarqube.groovy
+++ vars/sonarqube.groovy
@@ -24,7 +24,7 @@ node {
 	   sh 'mvn package'
 	   sh 'mvn install'
 	   sh 'mvn test'
-	   executeScanner('Sonar-4.2','Sonar-6')	         
+	   executeScanner('Sonar-4.2','Sonar-8')	         
    }
 
 
