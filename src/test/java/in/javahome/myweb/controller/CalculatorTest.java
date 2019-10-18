969e5a0691c23df8106af30ea9d193481b192457
sikandarqaisar <50927206+sikandarqaisar@users.noreply.github.com>
2019-10-19 00:24:41 +0500

    Update CalculatorTest.java

--- src/test/java/in/javahome/myweb/controller/CalculatorTest.java
+++ src/test/java/in/javahome/myweb/controller/CalculatorTest.java
@@ -1,53 +1,53 @@
-package in.javahome.myweb.controller;
+// package in.javahome.myweb.controller;
 
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Properties;
+// import java.io.FileOutputStream;
+// import java.io.IOException;
+// import java.io.OutputStream;
+// import java.util.Properties;
 
-import junit.framework.Assert;
-import junit.framework.TestCase;
+// import junit.framework.Assert;
+// import junit.framework.TestCase;
 
-public class CalculatorTest extends TestCase {
-	Calculator cal = new Calculator();
+// public class CalculatorTest extends TestCase {
+// 	Calculator cal = new Calculator();
 
-	public void testAdd() {
-		Properties prop = new Properties();
+// 	public void testAdd() {
+// 		Properties prop = new Properties();
 		
-		OutputStream output = null;
+// 		OutputStream output = null;
 
-		try
-		{
+// 		try
+// 		{
 
-			output = new FileOutputStream("../config.properties");
+// 			output = new FileOutputStream("../config.properties");
 
-			// set the properties value
-			prop.setProperty("database", "localhost");
-			prop.setProperty("dbuser", "hari");
-			prop.setProperty("dbpassword", "password");
+// 			// set the properties value
+// 			prop.setProperty("database", "localhost");
+// 			prop.setProperty("dbuser", "hari");
+// 			prop.setProperty("dbpassword", "password");
 
-			// save properties to project root folder
-			prop.store(output, null);
+// 			// save properties to project root folder
+// 			prop.store(output, null);
 
-		}catch(
-		IOException io)
-		{
-			io.printStackTrace();
-		}finally
-		{
-			if (output != null) {
-				try {
-					output.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
+// 		}catch(
+// 		IOException io)
+// 		{
+// 			io.printStackTrace();
+// 		}finally
+// 		{
+// 			if (output != null) {
+// 				try {
+// 					output.close();
+// 				} catch (IOException e) {
+// 					e.printStackTrace();
+// 				}
+// 			}
 
-		}
-	}
+// 		}
+// 	}
 
-	public void testMultiply() {
-		Assert.assertEquals(cal.multiply(10, 20), 200);
-	}
-}
+// 	public void testMultiply() {
+// 		Assert.assertEquals(cal.multiply(10, 20), 200);
+// 	}
+// }
