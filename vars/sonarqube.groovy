9fdcf8d625ca933b7582ae1b0f850e1898c1da3f
sikandarqaisar <50927206+sikandarqaisar@users.noreply.github.com>
2019-10-22 22:22:19 +0500

    Update sonarqube.groovy

--- vars/sonarqube.groovy
+++ vars/sonarqube.groovy
@@ -12,14 +12,14 @@ gitBranch=inputs.gitbranch
 EmailFrom=inputs.EmailFrom
 EmailTo=inputs.EmailTo
 node {
-   def sonarUrl = 'sonar.host.url=http://52.27.136.95'
+   def sonarUrl = 'sonar.host.url=http://34.212.168.221'
    def mvn = tool (name: 'maven3', type: 'maven') + '/bin/mvn'	
    stage('Sonar Publish'){
 	   sh 'mvn clean'
 	   sh 'mvn package'
 	   sh 'mvn install'
 	   sh 'mvn test'
-	   sh 'mvn sonar:sonar -Dsonar.host.url=http://52.27.136.95 -Dsonar.login=admin -Dsonar.password=0yakGXSe6Wwz'
+	   sh 'mvn sonar:sonar -Dsonar.host.url=http://34.212.168.221 -Dsonar.login=admin -Dsonar.password=0yakGXSe6Wwz'
 	   executeScanner('Sonar-4.2','sonar-8')	         
    }
 	stage('jacoco'){
